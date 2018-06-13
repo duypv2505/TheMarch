ab2eab582225a51b1a58cbc2f62b5e47baead264
Duy-Pham-S3 <duy.pham@s3corp.com.vn>
2018-06-13 11:04:52 +0700

    delete

--- TheMarch/common.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-def connect_db():
-    connection = MongoClient('ds016718.mlab.com', 16718)
-    db = connection['themarch']
-    db.authenticate('duypv', 'Pa$$w0rd1')    
-    return db
