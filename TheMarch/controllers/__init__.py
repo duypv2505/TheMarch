c1fb926cf240f0a49f40c15badacde6c8d4b204a
Duy <duypv250592@gmail.com>
2018-07-25 19:34:45 +0700

    delete source

--- TheMarch/controllers/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-import TheMarch.common as common
-
-#Get database
-common.connect_db()
-
-__all__ = [os.path.basename(
-    f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
